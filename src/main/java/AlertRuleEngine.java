8a9c913fffed7825210285f41c759bdc85638e7b
MarcelKrawczyk <marcelkrawczyk04@gmail.com>
2026-06-23 05:50:02 +0200

    add WARN rule for divisibility by 7

--- src/main/java/AlertRuleEngine.java
+++ src/main/java/AlertRuleEngine.java
@@ -8,10 +8,14 @@ public class AlertRuleEngine {
     private static void run() {
         RuleEngine ruleEngine = new RuleEngine(List.of(
                 new DivisibilityRule(3, "LOW"),
-                new DivisibilityRule(5, "ADVISORY")
+                new DivisibilityRule(5, "ADVISORY"),
+                new DivisibilityRule(7, "WARN")
         ));
         for (int value = 1; value <= 20; value++){
             System.out.println(ruleEngine.process(value));
         }
+        System.out.println(ruleEngine.process(21));
+        System.out.println(ruleEngine.process(35));
+        System.out.println(ruleEngine.process(105));
     }
 }
